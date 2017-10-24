00d4d0b1ee95b7010686b77473adc18430e45e33
vitaly simikov <vitaly.simikov@mifort.ort>
2017-10-24 17:32:26 +0300

    hot fix branch name

--- phase_1.js
+++ phase_1.js
@@ -1,12 +1,12 @@
-// var branch = require('git-branch');
-
-// branch(function(err, str) {
-//     if (err) throw err;
-//     console.log(str);
-//     //=> 'master' 
-//   });
 var git = require('git-rev-sync');
 
 console.log('short\thash\t'+git.short());
 console.log('long\thash\t'+git.long());
-console.log('branch\tname\t'+git.branch());
+//console.log('branch\tname\t'+git.branch());
+
+var branch = require('git-branch');
+
+branch(function(err, str) {
+    if (err) throw err;
+    console.log(str);
+  });
