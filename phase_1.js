03cccd84237c4fbe7d00c87259571170084826c7
vitaly simikov <vitaly.simikov@mifort.ort>
2017-10-25 12:35:23 +0300

    add packages for test

--- phase_1.js
+++ phase_1.js
@@ -2,11 +2,12 @@ var git = require('git-rev-sync');
 
 console.log('short\thash\t'+git.short());
 console.log('long\thash\t'+git.long());
-//console.log('branch\tname\t'+git.branch());
+console.log('branch\tname\t'+git.branch());
 
-var branch = require('git-branch');
+// var branch = require('git-branch');
 
-branch(function(err, str) {
-    if (err) throw err;
-    console.log(str);
-  });
+// branch(function(err, str) {
+//     if (err) throw err;
+//     console.log(str);
+//   });
+
